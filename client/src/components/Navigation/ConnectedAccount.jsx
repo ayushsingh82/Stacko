e908eb5d3d873235e418d351a16c956ff6e4b730
Ayush Singh <ayushsinghmi711@gmail.com>
2023-12-23 19:35:03 +0530

    connected network

--- client/src/components/Navigation/ConnectedAccount.jsx
+++ client/src/components/Navigation/ConnectedAccount.jsx
@@ -3,6 +3,9 @@ import Web3Context from "../../context/Web3Context"
 
 const ConnectedAccount=()=>{
   const {selectedAccount}=useContext(Web3Context);
-  console.log(selectedAccount)
+//   console.log(selectedAccount)
+return (
+    <p>Connnected Account:{selectedAccount}</p>
+)
 }
 export default ConnectedAccount
--- client/src/components/Navigation/ConnectedNetwork.jsx
+++ client/src/components/Navigation/ConnectedNetwork.jsx
@@ -2,6 +2,12 @@ import { useContext } from "react"
 import Web3Context from "../../context/Web3Context"
 
 const ConnectedNetwork=()=>{
+     const {chainId}=useContext(Web3Context);
+     if(chainId===11155111){
+        return <p>Connected Network : Sepolia</p>
+     }else{
+        return <p>Connected Netwrok : Unsupported</p>
+     }
 
 }
 
