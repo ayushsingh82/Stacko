db73722b6317ec9cc0069f59cb82ec67192f8838
Ayush Singh <ayushsinghmi711@gmail.com>
2023-12-24 19:24:13 +0530

    claim reward css

--- client/src/components/ClaimReward/ClaimReward.jsx
+++ client/src/components/ClaimReward/ClaimReward.jsx
@@ -2,7 +2,7 @@ import { useContext} from "react";
 import Web3Context from "../../context/Web3Context"
 import Button from "../Button/Button";
 import { toast } from "react-hot-toast";
-// import "./ClaimReward.css"
+import "./ClaimReward.css"
 
 const ClaimReward = ()=>{
  const {stakingContract}=useContext(Web3Context);
@@ -15,14 +15,14 @@ const ClaimReward = ()=>{
       success: 'Transaction successful 👌',
       error: 'Transaction failed 🤯'
     });
-    if(receipt.status === 1){
-        setTransactionStatus("Transaction Is Successful")
-        setTimeout(()=>{
-          setTransactionStatus("")
-        },5000) 
-      } else{
-        setTransactionStatus("Transaction failed. Please try again.");
-      }
+    // if(receipt.status === 1){
+    //     setTransactionStatus("Transaction Is Successful")
+    //     setTimeout(()=>{
+    //       setTransactionStatus("")
+    //     },5000) 
+    //   } else{
+    //     setTransactionStatus("Transaction failed. Please try again.");
+    //   }
   }catch(error){
     console.error("Claim Reward Failed",error.message)
   }
